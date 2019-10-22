0010efd87b94b8ded728f6937f3fb0c0382a72ca
clean-form-8028 <zamirahmo66@gmail.com>
2019-10-22 23:35:12 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
+require_relative '../lib/position_taken.rb'
 # code your #position_taken? method here!
+the_board = ["",""," ", " ", "X", " ", " ", " ",""]
+
+def #position_taken?(board, index)
+
+(board[index] == " " || board[index] == "" || board[index] == nil)
+end
+
+puts position_taken?(the_board, 0)
+puts position_taken?(the_board, 1)
+puts position_taken?(the_board, 2)
+puts position_taken?(the_board, 3)
+puts position_taken?(the_board, 4)
