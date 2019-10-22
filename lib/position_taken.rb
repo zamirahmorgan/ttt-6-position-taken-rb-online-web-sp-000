765c610d25c08d78e292b40f225f1d31659b00b3
clean-form-8028 <zamirahmo66@gmail.com>
2019-10-22 23:33:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,4 +3,11 @@ the_board = ["",""," ", " ", "X", " ", " ", " ",""]
 
 def #position_taken?(board, index)
 
+(board[index] == " " || board[index] == "" || board[index] == nil)
 end
+
+puts position_taken?(the_board, 0)
+puts position_taken?(the_board, 1)
+puts position_taken?(the_board, 2)
+puts position_taken?(the_board, 3)
+puts position_taken?(the_board, 4)
